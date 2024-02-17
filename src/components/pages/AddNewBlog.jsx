395db84b29706fef6907dbbaff837fa7981370c1
farish-jamal <farishjamal98@gmail.com>
2024-02-17 22:20:11 +0530

    add blog feature implemented

--- src/components/pages/AddNewBlog.jsx
+++ src/components/pages/AddNewBlog.jsx
@@ -2,11 +2,43 @@ import { useEffect, useState } from "react";
 import Footer from "../layout/Footer";
 import NavBar from "../layout/NavBar";
 import getUserFromJwt from "../../helper/getAccessToken";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function AddNewBlog() {
   const [user, setUser] = useState(null);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [bannerImage, setBannerImage] = useState(null);
+  const [category, setCategory] = useState("");
 
   const accessToken = localStorage.getItem("accessToken");
+  const navigateTo = useNavigate();
+
+  const handleCreateNewBlog = async (e) => {
+    e.preventDefault();
+    try {
+      const blog = await axios.post(
+        "http://localhost:3001/blog/create",
+        {
+          title,
+          author: user.id,
+          description,
+          bannerImage,
+          category,
+        },
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      navigateTo("/");
+      console.log(blog);
+    } catch (error) {
+      console.error("Error occurred during creating new blog:", error);
+    }
+  };
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -25,17 +57,44 @@ function AddNewBlog() {
       <NavBar user={user} />
       <div className="container my-4">
         <h3 className="text-center">Post A New Blog</h3>
-        <form>
+        <form onSubmit={handleCreateNewBlog}>
           <div className="mb-3">
             <label className="form-label">Title</label>
             <textarea
               className="form-control"
               name="description"
               rows="2"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
             ></textarea>
           </div>
           <div className="input-group my-3">
-            <input type="file" className="form-control" name="bannerImage" />
+            <input
+              type="file"
+              className="form-control"
+              name="bannerImage"
+              onChange={(e) => setBannerImage(e.target.files[0])}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Category</label>
+            <select
+              className="form-select"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <option value="">Select category</option>
+              <option value="Tech Updates">Tech Updates</option>
+              <option value="React Js">React Js</option>
+              <option value="JavaScript">JavaScript</option>
+              <option value="Python">Python</option>
+              <option value="Java">Java</option>
+              <option value="C#">C#</option>
+              <option value="C++">C++</option>
+              <option value="Ruby">Ruby</option>
+              <option value="PHP">PHP</option>
+              <option value="Swift">Swift</option>
+            </select>
           </div>
           <div className="mb-3">
             <label className="form-label">Write Blog</label>
@@ -43,6 +102,8 @@ function AddNewBlog() {
               className="form-control"
               name="content"
               rows="5"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
             ></textarea>
           </div>
           <button className="btn btn-success w-100">Post</button>
