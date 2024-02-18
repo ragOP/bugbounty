9af97e90d03c950ee603fd128a3461bb04698c60
farish-jamal <farishjamal98@gmail.com>
2024-02-19 01:03:10 +0530

    fetching all blogs on homepage

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -5,9 +5,11 @@ import Pagination from "../common/Pagination";
 import Footer from "../layout/Footer";
 import getUserFromJwt from "../../helper/getAccessToken";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 function HomePage() {
   const [user, setUser] = useState(null);
+  const [blogs, setBlogs] = useState([]);
 
   const accessToken = localStorage.getItem("accessToken");
 
@@ -20,18 +22,40 @@ function HomePage() {
         console.error("Error fetching user data:", error);
       }
     };
+    const getAllBlogs = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/blog/");
+        setBlogs(response.data);
+      } catch (error) {
+        console.error("Error occurred during fetching blogs:", error);
+      }
+    };
     if (accessToken) fetchUser();
+    getAllBlogs();
   }, [accessToken]);
 
   return (
     <div>
-      <>
-        <NavBar user={user} />
-        <Badegs />
-        <Cards />
-        <Pagination />
-        <Footer />
-      </>
+      <NavBar user={user} />
+      <Badegs />
+      <div className="d-flex align-items-center justify-content-center flex-wrap">
+        {blogs.map((blog) => (
+          <Cards
+            key={blog._id}
+            bannerImage={blog.bannerImage}
+            _id={blog._id}
+            title={blog.title}
+            content={blog.content}
+            category={blog.category}
+            totalViews={blog.totalViews}
+            readTime={blog.readTime}
+            createdAt={blog.createdAt}
+            author={blog.author}
+          />
+        ))}
+      </div>
+      <Pagination />
+      <Footer />
     </div>
   );
 }
